169793e72e3b931d67fdea5e41641cbfdc79a2f5
Mahrukhkhann <mahrukh.khan.official@gmail.com>
2026-08-10 23:44:38 +0500

    Create vintage-style digital clock UI

---
+++ script.js
@@ -0,0 +1,14 @@
+function updateClock() {
+    const now = new Date();
+
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    document.getElementById("time").textContent =
+        `${hours}:${minutes}:${seconds}`;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
